da5f9f235419eed71a455b875d6cf46fbf7757cf
manoo25 <mohamedsalamma25@gmail.com>
2025-06-20 02:13:38 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {styles} from './styles.js';
 
 export default function App() {
+
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text style={{fontSize: 20, fontWeight: 'bold', marginTop: 50}}>ToDo App</Text>
+     <TextInput style={styles.input} placeholder="Task Title"/>
+     <TextInput style={styles.input} placeholder="Description"/>
+     <TouchableOpacity style={styles.submitBtn}>
+       <Text style={{color: '#fff'}}>Add Task</Text>
+     </TouchableOpacity> 
+     <View style={styles.dividerLine}>
+      <Text style={styles.filterText}>Filter by:</Text>
+      <View style={styles.filterContainer}>
+        <TouchableOpacity style={styles.filterBtn}>
+          <Text style={styles.filterText}>All</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.filterBtn}>
+          <Text style={styles.filterText}>Active</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.filterBtn}>
+          <Text style={styles.filterText}>Completed</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
     </View>
+    
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ styles.js
@@ -0,0 +1,81 @@
+import { StyleSheet } from "react-native";
+
+export const styles = ({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingBottom: 20,
+    paddingTop:30,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#aeaeae",
+    width: "90%",
+    marginVertical: 10,
+    height: 50,
+    padding: 10,
+    borderRadius: 5,
+  },
+  submitBtn: {
+    width: "50%",
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    borderRadius: 10,
+  },
+  text: {
+    color: "white",
+    fontSize: 18,
+    textTransform: "uppercase",
+  },
+  dividerLine: {
+   flex:1,
+   paddingTop:20,
+   alignItems: "center",
+    width: "90%",
+    backgroundColor: "#aeaeae",
+    marginVertical: 15,
+  },
+  filterContainer: {
+    marginTop: 10,
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "space-between",
+  },
+  filterBtn: {
+    width: "30%",
+    backgroundColor: "#ffffff",
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  filterText: {
+    color: "black",
+    fontSize: 15,
+  },
+  activeFilterBtn: {
+    width: "30%",
+    backgroundColor: "black",
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  activeFilterText: {
+    color: "white",
+    fontSize: 15,
+  },
+  todosContainer: {
+    marginTop: 10,
+  },
+  doneTodo: {
+    textDecorationLine: "line-through",
+  },
+});
