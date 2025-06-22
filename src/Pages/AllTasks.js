bd2942c39f66fb1aa82eeb5e108fde821bc7f24e
manoo25 <mohamedsalamma25@gmail.com>
2025-06-22 16:34:46 +0300

    Day3 ..

---
+++ src/Pages/AllTasks.js
@@ -0,0 +1,19 @@
+
+import { styles } from "../../Styles/styles.js";
+import { Text, View } from "react-native";
+
+
+function AllTodos() {
+   
+    return ( 
+
+<View style={styles.container}>
+
+     <Text style={{marginTop:100,fontSize:50}}>All Tasks</Text>
+</View>
+       
+    
+     );
+}
+
+export default AllTodos;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,36 +1,73 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import Home from '../Pages/Home';
 import Details from '../Pages/Details';
+import AllTodos from '../Pages/AllTasks';
 
-export const PATHS={
-    Home:'Home Page',
-    DETAILS:'Details Page'
+export const PATHS = {
+  Home: 'Home Page',
+  DETAILS: 'Details Page',
+  AllTodos: 'All Tasks',
+};
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function StackScreen() {
+  return (
+    <Stack.Navigator 
+        screenOptions={{
+            headerShown: false, 
+        }}
+    >
+      <Stack.Screen name={PATHS.Home} component={Home} />
+      <Stack.Screen
+        name={PATHS.DETAILS}
+        component={Details}
+        options={{
+          headerShown: true,
+          headerTitle: '',
+          headerBackTitleVisible: true,
+          headerBackTitle: 'Back',
+          headerTintColor: 'green',
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
 
 function Router() {
-    const MyStack = createNativeStackNavigator();
-    return (  
-      <NavigationContainer>
-<MyStack.Navigator screenOptions={{
-headerShown: false,
-    }}>
-    <MyStack.Screen name={PATHS.Home} component={Home}></MyStack.Screen>
-    <MyStack.Screen name={PATHS.DETAILS} component={Details}
-    options={
-        {
-            headerShown:true,
-             headerTitle:'',
-            headerBackTitleVisible:true,
-            headerBackTitle:'Back',
-            headerTintColor:'green',
-           
-        }
-    }
-    ></MyStack.Screen>
-</MyStack.Navigator>
-      </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarActiveTintColor: 'green',
+          
+          tabBarShowLabel: false,
+
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? 'home' : 'home-outline';
+            } 
+            else if (route.name === PATHS.AllTodos) {
+              iconName = focused ? 'list' : 'list-outline';
+            }
+
+            return <Ionicons name={iconName} size={30} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={StackScreen} />
+        <Tab.Screen name={PATHS.AllTodos} component={AllTodos} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
+
 
 export default Router;
